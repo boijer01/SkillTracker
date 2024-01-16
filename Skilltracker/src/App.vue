b78d381458177ac638d3b3c17f8d08bc46fa3737
boijersi <boijersi@arcada.fi>
2024-01-16 12:13:56 +0200

    init

---
+++ Skilltracker/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+
+    <div class="wrapper">
+      <nav>
+        <!--<RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink> -->
+      </nav>
+    </div>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+
+body {
+  background-color: #080C26;
+}
+</style>
---
+++ Skilltracker/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+   
+  </main>
+</template>
+
+
+<style>
+
+body {
+  background-color: #080C26;
+}
+</style>
+
